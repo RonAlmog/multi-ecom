4771d7f9881a79ee4df4d5bab31325e701cf1e3b
Ron Almog <ron8132@gmail.com>
2025-04-15 20:26:08 -0400

    finish desktop menu

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -36,6 +36,10 @@ const navbarItems = [
     children: "Home",
   },
   {
+    href: "/about",
+    children: "About",
+  },
+  {
     href: "/pricing",
     children: "Pricing",
   },
@@ -68,6 +72,23 @@ export const Navbar = () => {
             {item.children}
           </NavbarItem>
         ))}
+      </div>
+
+      <div className="hidden lg:flex">
+        <Button
+          asChild
+          variant="secondary"
+          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors"
+        >
+          <Link href="/sign-in">Log in</Link>
+        </Button>
+        <Button
+          asChild
+          variant="secondary"
+          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors"
+        >
+          <Link href="/sign-up">Start selling</Link>
+        </Button>
       </div>
     </nav>
   );
