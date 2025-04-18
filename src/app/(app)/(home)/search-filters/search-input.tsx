bbaf1eb0912582d4da77a2520b40ad93f419e322
Ron Almog <ron8132@gmail.com>
2025-04-18 08:35:41 -0400

    responsive sidebar menu

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,14 +1,25 @@
+"use client";
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
-import React from "react";
+import { ListFilterIcon, SearchIcon } from "lucide-react";
+import React, { useState } from "react";
+import { CustomCategory } from "../types";
+import CategoriesSidebar from "./categories-sidebar";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   disabled?: boolean;
+  data: CustomCategory[];
 }
 
-const SearchInput = ({ disabled }: Props) => {
+const SearchInput = ({ disabled, data }: Props) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
     <div className="flex items-center gap-2 w-full">
+      <CategoriesSidebar
+        data={data}
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+      />
       <div className="relative w-full">
         <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-5 text-neutral-500" />
         <Input
@@ -17,6 +28,13 @@ const SearchInput = ({ disabled }: Props) => {
           disabled={disabled}
         />
       </div>
+      <Button
+        variant="elevated"
+        className="size-12 shrink-0 flex lg:hidden"
+        onClick={() => setIsSidebarOpen(true)}
+      >
+        <ListFilterIcon className="size-6" />
+      </Button>
       {/* TODO: add categories view */}
     </div>
   );
