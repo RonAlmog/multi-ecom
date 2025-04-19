83536322885b4fb78e6c5d04b6fea55c6faa8864
Ron Almog <ron8132@gmail.com>
2025-04-19 10:36:36 -0400

    use trpc

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,46 +1,27 @@
-import configPromise from "@payload-config";
-import { getPayload } from "payload";
-
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
-import { SearchFilters } from "./search-filters";
-import { Category } from "@/payload-types";
-import { CustomCategory } from "./types";
+import { SearchFilterLoading, SearchFilters } from "./search-filters";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Suspense } from "react";
 
 export default async function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const payload = await getPayload({
-    config: configPromise,
-  });
-  const data = await payload.find({
-    collection: "categories",
-    depth: 1,
-    pagination: false,
-    where: {
-      parent: {
-        exists: false,
-      },
-    },
-    sort: "name",
-  });
-  console.log({ data });
+  const queryClient = getQueryClient();
 
-  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
-    ...doc,
-    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
-      // depth is 1, so we are confident that doc is of type Category
-      ...(doc as Category),
-      subcategories: undefined,
-    })),
-  }));
-  console.log(data, formattedData);
+  void queryClient.prefetchQuery(trpc.categories.getMany.queryOptions());
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <SearchFilters data={formattedData} />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<SearchFilterLoading />}>
+          <SearchFilters />
+        </Suspense>
+      </HydrationBoundary>
       <div className="flex-1 bg-[#F4F4F0]">{children}</div>
       <Footer />
     </div>
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -2,24 +2,17 @@
 
 // for server pages:
 // import { getQueryClient, trpc } from "@/trpc/server";
-import { useQuery } from "@tanstack/react-query";
-import { useTRPC } from "@/trpc/client";
+// import { useQuery } from "@tanstack/react-query";
+// import { useTRPC } from "@/trpc/client";
 
 export default function Home() {
   // const queryClient = getQueryClient();
-  const trpc = useTRPC();
+  //
 
   // for server pages:
   // const categories = await queryClient.fetchQuery(
   //   trpc.categories.getMany.queryOptions()
   // );
 
-  const categories = useQuery(trpc.categories.getMany.queryOptions());
-  return (
-    <div className="flex flex-col gap-y-4 p-4">
-      {categories.isLoading && <div className="text-3xl">Loading...</div>}
-
-      {JSON.stringify(categories.data, null, 2)}
-    </div>
-  );
+  return <div className="flex flex-col gap-y-4 p-4">Home page</div>;
 }
